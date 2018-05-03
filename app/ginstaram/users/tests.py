3c104c0e3a412ebb6b2f1b06ac32859fadd5176f
Aram Mine <aram.mine@gaiax.com>
2018-05-03 16:30:01 +0900

    ユーザーIDという文字列が表示されるかのテストを追加

--- app/ginstaram/users/tests.py
+++ app/ginstaram/users/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.shortcuts import resolve_url
 
-# Create your tests here.
+class ViewTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/user/')
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "ユーザーID")
