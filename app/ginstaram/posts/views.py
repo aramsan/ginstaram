67e32aa9029d98e1e7e4eea1fb737124eaeb709c
Aram Mine <aram.mine@gaiax.com>
2018-05-09 01:31:52 +0900

    ダミーデータをタイムらãラタイムラインに表示

--- app/ginstaram/posts/views.py
+++ app/ginstaram/posts/views.py
@@ -1,4 +1,50 @@
 from django.shortcuts import render
 
 def timeline(request):
-    return render(request, 'posts/timeline.html')
+    post_list = [ 
+        {
+            'author':{'username':"user1",'picture':"https://placehold.jp/32x32.png"},
+            'picture':"https://placehold.jp/800x800.png",
+            'like':25,
+            'time':"2018-05-08T20:00:00Z+09",
+            'comment':[
+                { 'username':"user1", 'comment':"フォトジェニック"} ,
+                { 'username':"user2", 'comment':"面白い"} ,
+                { 'username':"user3", 'comment':"かっこいい"} ,
+            ],
+        },
+        {
+            'author':{'username':"user1",'picture':"https://placehold.jp/32x32.png"},
+            'picture':"https://placehold.jp/800x800.png",
+            'like':25,
+            'time':"2018-05-08T20:00:00Z+09",
+            'comment':[
+                { 'username':"user1", 'comment':"フォトジェニック"} ,
+                { 'username':"user2", 'comment':"面白い"} ,
+                { 'username':"user3", 'comment':"かっこいい"} ,
+            ],
+        },
+        {
+            'author':{'username':"user1",'picture':"https://placehold.jp/32x32.png"},
+            'picture':"https://placehold.jp/800x800.png",
+            'like':25,
+            'time':"2018-05-08T20:00:00Z+09",
+            'comment':[
+                { 'username':"user1", 'comment':"フォトジェニック"} ,
+                { 'username':"user2", 'comment':"面白い"} ,
+                { 'username':"user3", 'comment':"かっこいい"} ,
+            ],
+        },
+        {
+            'author':{'username':"user1",'picture':"https://placehold.jp/32x32.png"},
+            'picture':"https://placehold.jp/800x800.png",
+            'like':25,
+            'time':"2018-05-08T20:00:00Z+09",
+            'comment':[
+                { 'username':"user1", 'comment':"フォトジェニック"} ,
+                { 'username':"user2", 'comment':"面白い"} ,
+                { 'username':"user3", 'comment':"かっこいい"} ,
+            ],
+        },
+    ]
+    return render(request, 'posts/timeline.html', {'post_list':post_list})
