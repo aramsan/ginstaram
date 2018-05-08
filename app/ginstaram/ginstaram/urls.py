1f387e86939837927622e7d6788a81b187663453
Aram Mine <aram.mine@gaiax.com>
2018-05-09 00:40:40 +0900

    タイムライン決め打ちテンプレート作成

--- app/ginstaram/ginstaram/urls.py
+++ app/ginstaram/ginstaram/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('', include('ginstaram.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls')),
+    #path('accounts/', include('accounts.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
